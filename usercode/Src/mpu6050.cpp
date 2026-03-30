c2cb38c1936458d09085c8f179dfbde969827025
Wleaf666 <1617844769@qq.com>
2026-03-30 21:29:28 +0800

    fix:修复了欧拉角的结算错误，根据实际的小狗模型可能还要再次修改

--- usercode/Src/mpu6050.cpp
+++ usercode/Src/mpu6050.cpp
@@ -45,14 +45,14 @@ void MPU6050::Update()
     HAL_I2C_Mem_Read(hi2c, MPU_ADDR, REG_DATA, 1, data_buffer, 14, 100);
 
     int16_t RawAccleX = (int16_t)((data_buffer[0] << 8) | data_buffer[1]);
-    int16_t RawAccleY = ((int16_t)(data_buffer[2] << 8) | data_buffer[3]);
-    int16_t RawAccleZ = ((int16_t)(data_buffer[4] << 8) | data_buffer[5]);
+    int16_t RawAccleY = (int16_t)((data_buffer[2] << 8) | data_buffer[3]);
+    int16_t RawAccleZ = (int16_t)((data_buffer[4] << 8) | data_buffer[5]);
 
-    int16_t RawTemperature = ((int16_t)(data_buffer[6] << 8) | data_buffer[7]);
+    int16_t RawTemperature = (int16_t)((data_buffer[6] << 8) | data_buffer[7]);
 
-    int16_t RawGyroX = ((int16_t)(data_buffer[8] << 8) | data_buffer[9]);
-    int16_t RawGyroY = ((int16_t)(data_buffer[10] << 8) | data_buffer[11]);
-    int16_t RawGyroZ = ((int16_t)(data_buffer[12] << 8) | data_buffer[13]);
+    int16_t RawGyroX = (int16_t)((data_buffer[8] << 8) | data_buffer[9]);
+    int16_t RawGyroY = (int16_t)((data_buffer[10] << 8) | data_buffer[11]);
+    int16_t RawGyroZ = (int16_t)((data_buffer[12] << 8) | data_buffer[13]);
 
     this->accelX = RawAccleX / 8192.0f;
     this->accelY = RawAccleY / 8192.0f;
@@ -64,20 +64,21 @@ void MPU6050::Update()
 
     this->temptreture = RawTemperature / 340.0f + 35;
 
-    float accelPitch = atan2(this->accelX, sqrt(this->accelX * this->accelX + this->accelY * this->accelY + this->accelZ * this->accelZ)) *57.29578f;
+    float accelPitch = atan2(this->accelX, sqrt( this->accelY * this->accelY + this->accelZ * this->accelZ)) *57.29578f;
     float accelRoll = atan2(this->accelY, this->accelZ) * 57.29578f;
 
     uint32_t currentTick = osKernelGetTickCount();
     uint32_t deltaTick = currentTick - lastTick;
+    this->lastTick = currentTick;
 
     float dt = (float)deltaTick / (float)osKernelGetTickFreq();
 
     if(dt<=0.0f||dt>0.1f)
     {
-        dt = 0.2f;
+        dt = 0.02f;
     }
 
-    this->pitch = 0.98f * (this->pitch + this->gyroY * dt) + 0.02f * accelPitch;
+    this->pitch = 0.98f * (this->pitch + (-this->gyroY) * dt) + 0.02f * accelPitch;
     this->roll = 0.98f * (this->roll + this->gyroX * dt) + 0.02f * accelRoll;
 
     this->yaw = this->yaw + this->gyroZ * dt;
