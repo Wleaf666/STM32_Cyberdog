2ce4198caac4c61ccc8474a5d4ae6c473f981745
Wleaf666 <1617844769@qq.com>
2026-03-30 13:06:04 +0800

    chore:建立usercode目录树，隔离人为添加代码

--- usercode/Src/app.cpp
+++ usercode/Src/app.cpp
@@ -4,6 +4,7 @@
 
 // 以后所有的 C++ 头文件（比如你的 static_arena.hpp）都在这里尽情 include！
 uint32_t test_count = 0;
+uint32_t test_count1 = 0;
 
 void task_test(void *argument)
 {
@@ -15,10 +16,22 @@ void task_test(void *argument)
     }
 }
 
+void task_test1(void *argument)
+{
+    for (;;)
+    {
+        (*(uint32_t *)argument)++;
+        // HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+        osDelay(100);
+    }
+}
+
 void App_Main()
 {
     // 🎉 欢迎来到纯正的 C++17 世界！
     // 以后你的 FreeRTOS 任务创建、类的实例化、机器狗逻辑，全部写在这个函数里。
     osThreadAttr_t task_test_attr = {.priority = osPriorityNormal};
     osThreadNew(task_test, &test_count, &task_test_attr);
+    osThreadAttr_t task_test_attr1 = {.priority = osPriorityNormal};
+    osThreadNew(task_test1, &test_count1, &task_test_attr1);
 }
