96d0db67fe61884824ed864c0457c2f1b269891d
Wleaf666 <1617844769@qq.com>
2026-03-31 21:46:26 +0800

    feat:写了PCA6050的驱动

---
+++ usercode/Inc/pca9685.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "main.h"
+#include "cmsis_os2.h"
+
+enum class LegChanel : uint8_t
+{
+    FrontLeft = 0,
+    FrontRight = 1,
+    RearLeft = 2,
+    RearRight = 3
+};
+
+struct  ServoConfig
+{
+    float min_angle;
+    float max_angle;
+    uint16_t min_tick;
+    uint16_t max_tick;
+    uint16_t phase_offset;
+};
+
+
+class PCA9685
+{
+    private:
+        I2C_HandleTypeDef* hi2c;
+        uint8_t address;
+        osMutexId_t i2cMutex;
+
+        ServoConfig servoConfigs[4];
+
+        static constexpr uint8_t PCA9685_MODE1 = 0x00;
+        static constexpr uint8_t PCA9685_PRESCALE = 0xFE;
+        static constexpr uint8_t LED0_ON_L = 0x06;
+
+        bool writeRegister(uint8_t reg, uint8_t data);
+        uint8_t readRegister(uint8_t reg);
+    public:
+        PCA9685(I2C_HandleTypeDef *_hi2c, uint8_t _address = 0x80, osMutexId_t _i2cMutex = nullptr);
+
+        bool Init(float frequecy = 50.0f);
+
+        void setPWMFreq(float freq);
+
+        void setPWM(uint8_t channel, uint16_t on_tick, uint16_t off_tick);
+
+        void setAngle(LegChanel leg, float angle);
+
+        void setServoConfig(LegChanel leg, ServoConfig config);
+};
---
+++ usercode/Src/pca9685.cpp
@@ -0,0 +1,100 @@
+#include "pca9685.hpp"
+#include <cmath>
+#include "main.h"
+
+PCA9685::PCA9685(I2C_HandleTypeDef *_hi2c, uint8_t _address, osMutexId_t _i2cMutex ) : hi2c(_hi2c), address(_address), i2cMutex(_i2cMutex) 
+{
+    for (int i = 0; i < 4;i++)
+    {
+        servoConfigs[i] = {0.0f, 180.0f, 102, 512, static_cast<uint16_t>(i * 1000)};
+    }
+}
+
+bool PCA9685::Init(float frequency)
+{
+    writeRegister(PCA9685_MODE1, 0x20);
+    osDelay(5);
+
+    setPWMFreq(frequency);
+    return true;
+}
+
+void PCA9685::setPWMFreq(float freq)
+{
+    freq = (freq > 1000.0f) ? 1000.0f : ((freq < 24.0f) ? 24.0f : freq);
+
+    float prescaleval = 25000000.0f / (4096.0f * freq);
+    uint8_t prescale = static_cast<uint8_t>(std::round(prescaleval) - 1.0f);
+
+    uint8_t oldmode = readRegister(PCA9685_MODE1);
+    uint8_t newmode = (oldmode & 0x7F) | 0x10;
+
+    writeRegister(PCA9685_MODE1, newmode);
+    writeRegister(PCA9685_PRESCALE, prescale);
+    writeRegister(PCA9685_MODE1, oldmode);
+
+    osDelay(5);
+
+    writeRegister(PCA9685_MODE1, oldmode | 0x80);
+}
+
+void PCA9685::setPWM(uint8_t channel,uint16_t on_tick,uint16_t off_tick)
+{
+    uint8_t reg = LED0_ON_L + 4 * channel;
+    uint8_t data[4] = {static_cast<uint8_t>(on_tick & 0xFF),
+                       static_cast<uint8_t>(on_tick >> 8),
+                       static_cast<uint8_t>(off_tick & 0xFF),
+                       static_cast<uint8_t>(off_tick >> 8)};
+    if (i2cMutex != nullptr && osMutexAcquire(i2cMutex, osWaitForever) == osOK)
+    {
+        HAL_I2C_Mem_Write(hi2c, address, reg, 1, data, 4,100);
+        osMutexRelease(i2cMutex);
+    }
+}
+
+void PCA9685::setServoConfig(LegChanel leg,ServoConfig config)
+{
+    servoConfigs[static_cast<uint8_t>(leg)] = config;
+}
+
+void PCA9685::setAngle(LegChanel leg,float angle)
+{
+    uint8_t ch = static_cast<uint8_t>(leg);
+    ServoConfig &cfg = servoConfigs[ch];
+
+    angle = angle < cfg.min_angle ? cfg.min_angle : ((angle > cfg.max_angle) ? cfg.max_angle : angle);
+
+    float angleRatio = (angle - cfg.min_angle) / (cfg.max_angle - cfg.min_angle);
+    uint16_t pulse_width_ticks = static_cast<uint16_t>(cfg.min_tick + angleRatio * (cfg.max_tick - cfg.min_tick));
+
+    uint16_t on_tick = cfg.phase_offset;
+    uint16_t off_tick = (on_tick + pulse_width_ticks) % 4096;
+
+    setPWM(ch, on_tick, off_tick);
+}
+
+bool PCA9685::writeRegister(uint8_t reg,uint8_t data)
+{
+    bool success = false;
+    if(i2cMutex!=nullptr&&osMutexAcquire(i2cMutex,osWaitForever)==osOK)
+    {
+        if (HAL_I2C_Mem_Write(hi2c,address,reg,1,&data,1,100)==HAL_OK)
+        {
+            success = true;
+        }
+        osMutexRelease(i2cMutex);
+    }
+    return success;
+}
+
+uint8_t PCA9685::readRegister(uint8_t reg)
+{
+    uint8_t data = 0;
+    if (i2cMutex != nullptr && osMutexAcquire(i2cMutex, osWaitForever) == osOK)
+    {
+        HAL_I2C_Mem_Read(hi2c, address, reg, 1, &data, 1, 100);
+        osMutexRelease(i2cMutex);
+    }
+    return data;
+}
+
