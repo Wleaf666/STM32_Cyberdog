3da77fc2f90ae8bd1660fc9f29767dd66206a3e8
Wleaf666 <1617844769@qq.com>
2026-04-06 14:43:20 +0800

    test:本来想测su0.t语音模块，但因为板子还没到，麦克风和喇叭无法安装，故战且搁置在这里

--- usercode/Src/app.cpp
+++ usercode/Src/app.cpp
@@ -7,8 +7,10 @@
 #include "usart.h"
 #include "ssd1306.hpp"
 #include "stdio.h"
+#include "su03t.hpp"
 
 osMessageQueueId_t my_hc05_queue;
+osMessageQueueId_t my_su03t_queue = nullptr;
 
 // 以后所有的 C++ 头文件（比如你的 static_arena.hpp）都在这里尽情 include！
 uint32_t test_count = 0;
@@ -17,16 +19,60 @@ uint32_t test_count1 = 0;
 
 osMutexId_t i2c1_mutex = nullptr;
 osMutexId_t uart_mutex = nullptr;
+osMutexId_t uart2_mutex = nullptr; // 给串口2单独开一把锁
 
 MPU6050 *dogImu = nullptr;
 HC05 *blueTooth = nullptr;
 SSD1306 *oled = nullptr;
+SU03T *voiceModule = nullptr;
+
 
 void HC05_RxCallback_Wrapper(UART_HandleTypeDef *huart)
 {
     if (blueTooth != nullptr)
     {
         blueTooth->onRxCpltCallback();
+    }
+}
+
+void SU03T_RxCallback_Wrapper(UART_HandleTypeDef *huart)
+{
+    if (voiceModule != nullptr)
+    {
+        voiceModule->onRxCpltCallback();
+    }
+}
+
+void task_voice_handler(void *argument)
+{
+    for (;;)
+    {
+        VoiceCmd cmd = voiceModule->getCommand();
+
+        if (cmd != VoiceCmd::NONE)
+        {
+            oled->Clear();
+            oled->DrawString(0, 48, "Voice Command:");
+
+            switch (cmd)
+            {
+            case VoiceCmd::WAKE_UP:
+                oled->DrawString(0, 64, "Status: Awake");
+                break;
+            case VoiceCmd::FORWARD:
+                oled->DrawString(0, 64, "Action: Forward");
+                break;
+            case VoiceCmd::SIT_DOWN:
+                oled->DrawString(0, 64, "Action: Sit Down");
+                break;
+            default:
+                oled->DrawString(0, 64, "Unknown CMD");
+                break;
+            }
+            oled->Update();
+        }
+        oled->DrawString(0, 48, "NO Command:");
+        osDelay(20); // 必须有 Delay，让出 CPU
     }
 }
 
@@ -91,21 +137,27 @@ void App_Main()
 
     i2c1_mutex = osMutexNew(NULL);
     uart_mutex = osMutexNew(NULL);
+    uart2_mutex = osMutexNew(NULL);
     oled = new SSD1306(&hi2c1, i2c1_mutex);
-
+    voiceModule = new SU03T(&huart2, uart2_mutex);
     dogImu = new MPU6050(&hi2c1, i2c1_mutex);
     blueTooth = new HC05(&huart1, uart_mutex);
     my_hc05_queue = osMessageQueueNew(64, sizeof(uint8_t), NULL);
+    my_su03t_queue = osMessageQueueNew(32, sizeof(uint8_t), NULL);
 
     oled->Init();
     // dogImu->Init();
     blueTooth->Init(my_hc05_queue);
+    voiceModule->Init(my_su03t_queue);
 
     osThreadAttr_t task_bluetooth_test_attr = {.priority = osPriorityNormal};
     osThreadAttr_t task_test_attr1 = {.priority = osPriorityNormal};
     osThreadAttr_t task_display_attr = {.priority = osPriorityNormal};
+    osThreadAttr_t task_voice_attr = {.priority = osPriorityNormal};
 
     osThreadNew(task_bluetooth_test, &test_count, &task_bluetooth_test_attr);
     osThreadNew(task_display, NULL, &task_display_attr);
     osThreadNew(task_test1, &test_count1, &task_test_attr1);
+
+    osThreadNew(task_voice_handler, NULL, &task_voice_attr);
 }
