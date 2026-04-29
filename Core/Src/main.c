be384bce24ea7f040be8e2a86fe4039a1c615eac
Wleaf666 <1617844769@qq.com>
2026-04-29 19:10:15 +0800

    feat:增加了一个spi

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,6 +21,7 @@
 #include "cmsis_os.h"
 #include "adc.h"
 #include "i2c.h"
+#include "spi.h"
 #include "usart.h"
 #include "gpio.h"
 
@@ -95,6 +96,7 @@ int main(void)
   MX_USART1_UART_Init();
   MX_USART2_UART_Init();
   MX_ADC1_Init();
+  MX_SPI1_Init();
   /* USER CODE BEGIN 2 */
   /* USER CODE END 2 */
 
